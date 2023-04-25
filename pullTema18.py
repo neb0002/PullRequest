70c65eab6efc9622364a977358a20df974fef166
unknown <gabrielmahomed24@gmail.com>
2023-04-25 15:55:03 +0200

    First pull

---
+++ pullTema18.py
@@ -0,0 +1,11 @@
+class Circulo:
+    _radio = 0.0
+
+    def __init__(self, radio: float):
+        self._radio = radio
+
+    def area(self):
+        pass
+
+    def circunferencia(self):
+        pass
